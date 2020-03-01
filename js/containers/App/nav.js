aafad2a22d19899eeb8a68c41f09f8749210f5f9
Harish Rana <r.shivi@yahoo.com>
2020-03-01 15:52:33 +0530

    feat: add tab navigation

--- js/containers/App/nav.js
+++ js/containers/App/nav.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from '../Home';
+import Tab1Screen from '../Tab1Screen';
+import Tab2Screen from '../Tab2Screen';
+
+const Tab = createBottomTabNavigator();
+
+const ExampleTabNavigation = () => (
+  <Tab.Navigator>
+    <Tab.Screen name="Tab1Screen" component={Tab1Screen}></Tab.Screen>
+    <Tab.Screen name="Tab2Screen" component={Tab2Screen}></Tab.Screen>
+  </Tab.Navigator>
+);
 
 const Stack = createStackNavigator();
 
@@ -9,6 +21,7 @@ const Nav = () => (
   <NavigationContainer>
     <Stack.Navigator>
       <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="NavPlayground" component={ExampleTabNavigation} />
     </Stack.Navigator>
   </NavigationContainer>
 );
---
+++ js/containers/Tab2Screen/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import { BLACK, WHITE } from '../../styles';
+
+const Screen2 = () => (
+  <View style={styles.root}>
+    <Text style={styles.text}>
+      Screen 2
+    </Text>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    color: BLACK,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: WHITE,
+  },
+});
+
+export default Screen2;
