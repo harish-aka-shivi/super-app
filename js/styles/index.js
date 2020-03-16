e8ed6cbdecfb05c581fb22d227ceaaf181d7041c
Harish Rana <r.shivi@yahoo.com>
2020-03-16 17:26:50 +0530

    feat: make an ipod classic music app UI. (#6)
    * feat: add basic iPod custom navigator
    
    * feat: add the iPod classic wheel
    
    * feat: make functioning iPod click wheel
      - Add the Reactotron library
      - Implement gesture handler and react-native reanimated library
    
    * feat: Add list scrolling from the click wheel
    
    * fix: fix scrolling behavior partial
    
    * fix: make list scrollable in react native

--- js/styles/index.js
+++ js/styles/index.js
@@ -6,7 +6,9 @@
 
 export const BLACK = '#000000';
 export const WHITE = '#FFFFFF';
-
+export const RED = 'red';
+export const GRAY = '#C1C7D2';
 
 // Generic components color
 export const SEPARATOR = '#E0E0E0';
+export const CENTER_WHEEL_COLOR = '#323232';
